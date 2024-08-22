81f2290ff31f9f57cda1fee9fc8fc51d75f847a0
grubor-maja <maja.grubor@outlook.com>
2024-08-22 10:48:44 +0200

    Add routes for reservation and shelf

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const {User, validateUser} = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
+const {Book} = require('../models/bookModel');
 
 exports.registerUser = async(req,res) => {
     const {error} = validateUser(req.body);
@@ -33,4 +34,45 @@ exports.loginUser = async(req,res) => {
 exports.getUser = async(req,res) => {
     const user = await User.findById(req.user._id).select('-lozinka');
     res.send(user);
+}
+
+exports.addBookToReservations = async(req,res) => {
+    try {
+        const user = await User.findById(req.user._id);
+        if(user.rezervacije.length>=5) {
+            return res.status(400).send('Ne mozete rezervisati vise od 5 knjiga.')
+        }
+        const book = await Book.findById(req.body.bookId);
+        if(!book) return res.status(404).send('Knjiga nije pronadjena');
+
+        const slobodnaSignatura = book.signatura.find(sig => sig.status === 'slobodan');
+        if(!slobodnaSignatura) {
+            return res.status(400).send('Svi primerci ove knjige su zauzeti');
+
+        }
+        slobodnaSignatura.status = 'zauzet';
+
+        user.rezervacije.push(book._id);
+        await user.save()
+        await book.save();
+        res.send(user);
+    } catch (error) {
+        console.error("Greska na serveru:", error.message);
+        res.status(500).send('Greska na serveru');
+    }
+};
+
+exports.addBookToShelf = async(req,res) => {
+    try {
+        const user = await User.findById(req.user._id);
+        const book = await Book.findById(req.body.bookId);
+        if(!book) return res.status(400).send('Knjiga nije pronadjena');
+       
+        user.polica.push(book._id);
+        await user.save();
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send('Greska na serveru');
+    }
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getUser} = require('../controllers/userController');
+const {getUser, addBookToReservations,addBookToShelf} = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
 router.get('/me',auth,getUser);
+router.post('/reservations',auth,addBookToReservations);
+router.post('/shelf',auth,addBookToShelf);
 
 module.exports = router;
